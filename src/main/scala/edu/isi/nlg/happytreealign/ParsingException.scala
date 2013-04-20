7faf42e167222884db0dfa660ad8d00b4541d927
Tom Yubing Dong <tom.tung.dyb@gmail.com>
2013-04-19 20:47:40 -0700

    Update ParsingException to be Java6-compatible

--- src/main/scala/edu/isi/nlg/happytreealign/ParsingException.scala
+++ src/main/scala/edu/isi/nlg/happytreealign/ParsingException.scala
@@ -1,7 +1,5 @@
 package edu.isi.nlg.happytreealign
 
 class ParsingException(message: String = null,
-                       cause: Throwable = null,
-                       enableSuppression: Boolean = false,
-                       writableStackTrace: Boolean = false)
-  extends Exception(message, cause, enableSuppression, writableStackTrace)
+                       cause: Throwable = null)
+  extends Exception(message, cause)
